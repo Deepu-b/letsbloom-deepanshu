f62decec5fdd2a3c9abf64c60ab1bef24c13d786
Deepanshu Budhraja <deepanshubudhraja@gmail.com>
2023-12-07 14:08:52 +0530

    finalized stuff

--- letsbloom/library/views.py
+++ letsbloom/library/views.py
@@ -11,6 +11,7 @@ def getAllBooks(request):
     books = [i for i in  Book.objects.all().values()]
     return JsonResponse({'books ': books})
 
+
 @require_http_methods(["POST"])
 def addBook(request):
     try:
@@ -18,12 +19,15 @@ def addBook(request):
         name = book_data['name']
         book_id = book_data['id']
         author = book_data['author']
+        if Book.objects.filter(book_id=book_id).exists():
+            return JsonResponse({'scucess' : False, 'message' : "Book already exists"})
         new_book = Book.objects.create(name=name,book_id=book_id,author=author)
         new_book.save()
-        return JsonResponse({'success' :True})
+        return JsonResponse({'success' :True, 'message' : "Book added"})
     except Exception as err:
         JsonResponse({ 'success' : False ,'message' : "Too few details"})
-        
+      
+  
 @require_http_methods(["PATCH"])
 def updateBook(request):
     try:
@@ -31,7 +35,6 @@ def updateBook(request):
         id = book_data['id']
         name = book_data['name']
         author = book_data['author']
-        print(book_data)
         if id is not None:
             temp_book = Book.objects.get(id=id)
             if name is not None:
@@ -39,12 +42,12 @@ def updateBook(request):
             if author is not None:
                 setattr(temp_book,'author', author)
             temp_book.save()
-            JsonResponse({'success' : True})
+            return JsonResponse({'success' : True, 'message' : "Book data updated" })
         else:
-            JsonResponse({'success' : False, 'error' : "Book id not provided"})
+            return JsonResponse({'success' : False, 'error' : "Book id not provided"})
         
     except Book.DoesNotExist:
-        JsonResponse({'success' : False, 'error' : "Book doesnot exists"},status = 400)
+        JsonResponse({'success' : False, 'error' : "Book doesnot exists"})
     except json.JSONDecodeError:
-        return JsonResponse({'error': 'Invalid JSON data'}, status=400)
+        return JsonResponse({ 'success' : False ,'error': 'Invalid JSON data'})
     
